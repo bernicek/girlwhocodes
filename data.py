2c1148e2ace5aaf906f28680b1d423bfa4d291f2
Bernice Kariuki <bancyjimy@gmail.com>
2018-01-09 11:14:07 +0300

    Create data.py

---
+++ data.py
@@ -0,0 +1,29 @@
+def Articles():
+	articles = [
+	{
+	  'id':1,
+	  'tittle':'Article one',
+	  'body':'Everything is possible. When you need to acheive anything with determination, courage and by being open minded, you are more likely to acheive your goal.',
+	  'author':'Bernie',
+      'create_date':'01-10-2017',
+	},
+    
+    {
+	  'id':2,
+	  'tittle':'Article two',
+	  'body':'Everything is possible. When you need to acheive anything with determination, courage and by being open minded, you are more likely to acheive your goal.',
+	  'author':'Bancy',
+      'create_date':'01-10-2017',
+	},
+
+	{
+	  'id':3,
+	  'tittle':'Article three',
+	  'body':'Everything is possible. When you need to acheive anything with determination, courage and by being open minded, you are more likely to acheive your goal.',
+	  'author':'Benadate',
+      'create_date':'01-10-2017',
+	},	
+
+	]
+
+	return articles
